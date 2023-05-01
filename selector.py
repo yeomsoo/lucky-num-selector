1b3f9601fc3ff672ea2afc29d783a4c8c9772ad3
yeomsoo <jakoe3@gmail.com>
2023-05-01 14:39:23 +0900

    refactor : Functionalize sampling process

--- selector.py
+++ selector.py
@@ -1,6 +1,10 @@
 import random
 
-result = random.sample(range(1,45+1), k=6)
-print(result)
+
+def get_lucky_nums():
+    return random.sample(range(1,45+1), k=6)
+
+if __name__ == '__main__':
+    print(get_lucky_nums())
 
 
